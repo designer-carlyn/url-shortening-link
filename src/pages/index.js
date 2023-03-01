4b35830e64eb2319aa1098821a921e761357b6f8
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-03-01 14:34:33 +0800

    added readme and comment the shrten code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,67 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+// import axios from "axios";
+
+import "../scss/layout/index.scss";
 
 const IndexPage = () => {
-  const [originalLink, setOriginalLink] = useState("");
-  const [url, setUrl] = useState("");
-  const [shortenLink, setShortLink] = useState("");
-  const [linkList, setLinkList] = useState([]);
+  // const [originalLink, setOriginalLink] = useState("");
+  // const [url, setUrl] = useState("");
+  // const [shortenLink, setShortLink] = useState("");
+  // const [linkList, setLinkList] = useState([]);
 
-  const API_LINK = "https://api.shrtco.de/v2/shorten?url=";
+  // const API_LINK = "https://api.shrtco.de/v2/shorten?url=";
 
-  function handleChange(event) {
-    let value = event.target.value;
-    setOriginalLink(value);
-    setUrl(API_LINK + value);
-  }
+  // function handleChange(event) {
+  //   let value = event.target.value;
+  //   setOriginalLink(value);
+  //   setUrl(API_LINK + value);
+  // }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    getShortLink();
-  }
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   getShortLink();
+  // }
 
-  const copyLink = (value) => {
-    navigator.clipboard.writeText(value);
-  };
+  // const copyLink = (value) => {
+  //   navigator.clipboard.writeText(value);
+  // };
 
-  const deleteLink = (id) => {
-    setLinkList((prevList) => {
-      return prevList.filter((linkItem, index) => {
-        return index != id;
-      });
-    });
-  };
+  // const deleteLink = (id) => {
+  //   setLinkList((prevList) => {
+  //     return prevList.filter((linkItem, index) => {
+  //       return index != id;
+  //     });
+  //   });
+  // };
 
-  async function getShortLink() {
-    try {
-      const response = await axios.get(url);
-      setShortLink(response.data.result["full_short_link"]);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // async function getShortLink() {
+  //   try {
+  //     const response = await axios.get(url);
+  //     setShortLink(response.data.result["full_short_link"]);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
 
-  useEffect(() => {
-    if (shortenLink !== "") {
-      setLinkList([
-        ...linkList,
-        {
-          fullLink: originalLink,
-          shortLink: shortenLink,
-        },
-      ]);
-      setOriginalLink("");
-      setUrl("");
-      setShortLink("");
-    }
-  }, [shortenLink, linkList, originalLink]);
+  // useEffect(() => {
+  //   if (shortenLink !== "") {
+  //     setLinkList([
+  //       ...linkList,
+  //       {
+  //         fullLink: originalLink,
+  //         shortLink: shortenLink,
+  //       },
+  //     ]);
+  //     setOriginalLink("");
+  //     setUrl("");
+  //     setShortLink("");
+  //   }
+  // }, [shortenLink, linkList, originalLink]);
 
   return (
     <main>
-      <form onSubmit={handleSubmit}>
+      <h1>THIS IS HOMEPAGE</h1>
+      {/* <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleChange} />
         <button>Shorten It!</button>
       </form>
@@ -71,11 +74,11 @@ const IndexPage = () => {
             <button onClick={() => deleteLink(index)}>Delete</button>
           </div>
         );
-      })}
+      })} */}
     </main>
   );
 };
 
 export default IndexPage;
 
-export const Head = () => <title>Home Page</title>;
+export const Head = () => <title>URL Shortening Link</title>;
