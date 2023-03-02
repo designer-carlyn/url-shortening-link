54d06a64b58140e76e4eadb8c1b1fb6ee2aa2d86
mpotech-segovia <carlosegovia_mktg@mpotech.net>
2023-03-02 12:47:41 +0800

    shorten for code link and counter

--- src/components/shorten.js
+++ src/components/shorten.js
@@ -1,15 +1,76 @@
-import React from "react";
-import { Link } from "gatsby";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Shorten = () => {
+  const [originalLink, setOriginalLink] = useState("");
+  const [url, setUrl] = useState("");
+  const [shortenLink, setShortLink] = useState("");
+  const [linkList, setLinkList] = useState([]);
+
+  const [counter, setCounter] = useState(0);
+
+  const API_LINK = "https://api.shrtco.de/v2/shorten?url=";
+
+  function handleChange(event) {
+    let value = event.target.value;
+    setOriginalLink(value);
+    setUrl(API_LINK + value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    getShortLink();
+    setCounter((count) => count + 1);
+  }
+
+  const copyLink = (value, index) => {
+    navigator.clipboard.writeText(value);
+    console.log(index);
+  };
+
+  // const deleteLink = (id) => {
+  //   setLinkList((prevList) => {
+  //     return prevList.filter((linkItem, index) => {
+  //       return index != id;
+  //     });
+  //   });
+  // };
+
+  async function getShortLink() {
+    try {
+      const response = await axios.get(url);
+      setShortLink(response.data.result["full_short_link"]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    if (shortenLink !== "") {
+      setLinkList([
+        ...linkList,
+        {
+          fullLink: originalLink,
+          shortLink: shortenLink,
+          copyLink: false,
+          id: counter,
+        },
+      ]);
+      setOriginalLink("");
+      setUrl("");
+      setShortLink("");
+    }
+  }, [shortenLink, linkList, originalLink, counter]);
+
   return (
     <section className="shorten">
       <div className="container">
         <div className="shorten__container">
           <div className="shorten__container-holder">
-            <form className="shorten__container-form">
+            <form onSubmit={handleSubmit} className="shorten__container-form">
               <div className="input-group">
                 <input
+                  onChange={handleChange}
                   type="text"
                   className="input-link"
                   placeholder="Shorten a link here..."
@@ -19,15 +80,24 @@ const Shorten = () => {
               <button className="btn btn--lg">Shorten It</button>
             </form>
             <div className="shorten__container-list">
-              <div className="list-item">
-                <div className="item-original-link">
-                  https://www.frontendmentor.io
-                </div>
-                <div className="item-shorten-link">
-                  <Link to="shrtco.de/KCveN">shrtco.de/KCveN</Link>
-                </div>
-                <button className="btn btn--sm">Copy</button>
-              </div>
+              {linkList.map((link) => {
+                return (
+                  <div className="list-item" key={link.id}>
+                    <div className="item-original-link">{link.fullLink}</div>
+                    <div className="item-shorten-link">
+                      <a href={link.shortLink} target="__blank">
+                        {link.shortLink}
+                      </a>
+                    </div>
+                    <button
+                      className="btn btn--sm"
+                      onClick={() => copyLink(link.shortLink)}
+                    >
+                      Copy
+                    </button>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,79 +14,11 @@ import "../scss/layout/index.scss";
 import "../scss/pages/home.scss";
 
 const IndexPage = () => {
-  // const [originalLink, setOriginalLink] = useState("");
-  // const [url, setUrl] = useState("");
-  // const [shortenLink, setShortLink] = useState("");
-  // const [linkList, setLinkList] = useState([]);
-
-  // const API_LINK = "https://api.shrtco.de/v2/shorten?url=";
-
-  // function handleChange(event) {
-  //   let value = event.target.value;
-  //   setOriginalLink(value);
-  //   setUrl(API_LINK + value);
-  // }
-
-  // function handleSubmit(event) {
-  //   event.preventDefault();
-  //   getShortLink();
-  // }
-
-  // const copyLink = (value) => {
-  //   navigator.clipboard.writeText(value);
-  // };
-
-  // const deleteLink = (id) => {
-  //   setLinkList((prevList) => {
-  //     return prevList.filter((linkItem, index) => {
-  //       return index != id;
-  //     });
-  //   });
-  // };
-
-  // async function getShortLink() {
-  //   try {
-  //     const response = await axios.get(url);
-  //     setShortLink(response.data.result["full_short_link"]);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   if (shortenLink !== "") {
-  //     setLinkList([
-  //       ...linkList,
-  //       {
-  //         fullLink: originalLink,
-  //         shortLink: shortenLink,
-  //       },
-  //     ]);
-  //     setOriginalLink("");
-  //     setUrl("");
-  //     setShortLink("");
-  //   }
-  // }, [shortenLink, linkList, originalLink]);
-
   return (
     <main>
       <Header />
       <HeroBanner />
       <Shorten />
-      {/* <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} />
-        <button>Shorten It!</button>
-      </form>
-      {linkList.map((item, index) => {
-        return (
-          <div key={index}>
-            <h1>{item.fullLink}</h1>
-            <h2>{item.shortLink}</h2>
-            <button onClick={() => copyLink(item.shortLink)}>Copy</button>
-            <button onClick={() => deleteLink(index)}>Delete</button>
-          </div>
-        );
-      })} */}
     </main>
   );
 };
